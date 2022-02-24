794728c94f973c68a0800020eeb9fbf9f4a9abb0
DESKTOP-6KBPV3D\user <syr96@naver.com>
2022-02-24 19:44:09 +0900

    최초등록

---
+++ src/test/java/com/yullmaster/cleanArea/CleanAreaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yullmaster.cleanArea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanAreaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
